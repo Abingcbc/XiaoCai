25e3419ba403ea943f4b35e609d1c53ae42c87f9
cyy1998 <caiyiyang1998@126.com>
2019-06-18 17:11:09 +0800

    play music

--- music.py
+++ music.py
@@ -1,19 +1,30 @@
 from point_location import getPointLocation
+import pygame.mixer as pmx
+import pygame
 import cv2
 
+pygame.init()
+
+def gestureRecognition(mc):
+	pass
+
+
 def playMusic():
-    now_status_x=0
-    now_status_y=0
+    now_status_x=1
+    now_status_y=1
     last_status_x=0
     last_status_y=0
-    cap=cv2.VideoCapture(1)
+    cap=cv2.VideoCapture(0)
     while True:
         mc,frame,_,_=getPointLocation(cap)
         width=int(frame.shape[0])
         length=int(frame.shape[1])
-        if len(mc)==1:
+        if len(mc)==0:
+            last_status_y=0
+            last_status_x=0
+        if len(mc)>=1:
             point=mc[0]
-            #判断x轴区域
+
             if point[0]<=length/4:
                 now_status_x=1
             elif point[0]<=length/2:
@@ -22,7 +33,7 @@ def playMusic():
                 now_status_x=3
             elif point[0]<=length:
                 now_status_x=4
-            #判断y轴区域
+
             if point[1]<=width/4:
                 now_status_y=1
             elif point[1]<=width/2:
@@ -32,7 +43,8 @@ def playMusic():
             elif point[1]<=width:
                 now_status_y=4
             if now_status_x!=last_status_x or now_status_y!=last_status_y:
-                
+                meme=pmx.Sound('data/music/'+str(now_status_x)+'_'+str(now_status_y)+'.wav')
+                meme.play()
             last_status_x=now_status_x
             last_status_y=now_status_y
         for e in mc:
