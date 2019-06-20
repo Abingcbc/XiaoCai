f3c8fd1e1184755e924c731275ee61aa295865ef
cbc <19990626.love@163.com>
2019-06-20 23:08:03 +0800

    Update play_previous_music.py

--- play_previous_music.py
+++ play_previous_music.py
@@ -1,12 +1,21 @@
 import pygame
 import time
+import cv2
+from GestureRecognition import GestureRecognition
+from point_location import getPointLocation
 
 def run():
     with open('resource/music.txt') as file:
         s = file.readline()
     sound_list = s.split(' ')
     pygame.mixer.init()
+    gensture_recognition = GestureRecognition()
+    cap = cv2.VideoCapture(0)
     for beat in sound_list:
         s = pygame.mixer.Sound('data/music/'+beat+'.wav')
         s.play()
         time.sleep(1)
+        points = getPointLocation(cap)
+        if gensture_recognition.recognize(points):
+            return -1
+    return 1
